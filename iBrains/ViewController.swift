511d5835069ebfd71ef23c5a81053b7a45b22e0b
rlindskog <r.lindskog17@gmail.com>
2015-07-11 14:47:22 -0700

    Add Copywrite

--- iBrains/ViewController.swift
+++ iBrains/ViewController.swift
@@ -1,3 +1,11 @@
+//
+//  ViewController.swift
+//  iBrains
+//
+//  Created by Ryan on 7/11/15.
+//  Copyright (c) 2015 rlindskog. All rights reserved.
+//
+
 import UIKit
 
 class ViewController: UIViewController {
